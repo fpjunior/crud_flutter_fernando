df81e496fae60eee03a507c262e3f8a59c4724f1
fpjunior <fpjuniorpx@gmail.com>
2022-01-05 21:47:02 -0300

    mudança na posiçao do botao adicionar novo produto

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,6 @@ import 'package:flutter/material.dart';
 
 import 'core/colors.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   Future<FirebaseApp> app = Firebase.initializeApp();
-
-//   if (app == null) {
-//     app = Firebase.initializeApp();
-//     await Firebase.initializeApp(
-//       // Replace with actual values
-//       options: FirebaseOptions(
-//         apiKey: "XXX",
-//         appId: "1:634861432791:android:f4a163ea4b78e16ebf2592",
-//         messagingSenderId: "XXX",
-//         projectId: "crud-27136",
-//       ),
-//     );
-//   } else {
-//     app; // if already initialized, use that one
-//   }
-//   runApp(MyApp());
-// }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -86,169 +66,169 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.kBackgroundColor,
+      appBar: AppBar(
         backgroundColor: AppColors.kBackgroundColor,
-        appBar: AppBar(
-          backgroundColor: AppColors.kBackgroundColor,
-          title: customSearchBar,
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  searchResult = '';
+        title: customSearchBar,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                searchResult = '';
 
-                  if (customIcon.icon == Icons.search) {
-                    customIcon = const Icon(Icons.cancel);
-                    // Perform set of instructions.
-                    customSearchBar = ListTile(
-                      leading: Icon(
-                        Icons.search,
-                        color: Colors.white,
-                        size: 28,
-                      ),
-                      title: TextFormField(
-                        onChanged: (text) {
-                          setState(() {
-                            searchResult = text;
-                          });
-                        },
-                        decoration: InputDecoration(
-                          hintText: 'digite o nome do produto',
-                          hintStyle: TextStyle(
-                            color: Colors.white24,
-                            fontSize: 15,
-                            fontStyle: FontStyle.italic,
-                          ),
-                          border: InputBorder.none,
+                if (customIcon.icon == Icons.search) {
+                  customIcon = const Icon(Icons.cancel);
+                  // Perform set of instructions.
+                  customSearchBar = ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    title: TextFormField(
+                      onChanged: (text) {
+                        setState(() {
+                          searchResult = text;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'digite o nome do produto',
+                        hintStyle: TextStyle(
+                          color: Colors.white24,
+                          fontSize: 15,
+                          fontStyle: FontStyle.italic,
                         ),
-                        style: TextStyle(
-                          color: Colors.white,
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                } else {
+                  setState(() {
+                    customIcon = const Icon(Icons.search);
+                    customSearchBar = const Text('Lista de Produtos');
+                  });
+                }
+              });
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+          future: db.read(),
+          builder: (context, index) {
+            if (index.connectionState == ConnectionState.done) {
+              if (index.data == null) {
+                return Center(child: Text("Erro ao carregar dados"));
+              } else {
+                if (docs.isEmpty == 0) {
+                  return Center(child: Text("Nenhum produto encontrado"));
+                }
+
+                List list = [];
+
+                if (docs.isNotEmpty) {
+                  for (dynamic item in docs) {
+                    String name = item['name'].toString().toLowerCase();
+                    if (name.contains(searchResult.toLowerCase())) {
+                      list.add(item);
+                    }
+                  }
+                } else {
+                  list.addAll(docs);
+                }
+                return ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return new Container(
+                      padding: new EdgeInsets.only(right: 13.0),
+                      child: Card(
+                        semanticContainer: true,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        elevation: 5,
+                        color: AppColors.kColorCard,
+                        margin: EdgeInsets.all(10),
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => EditarProduto(
+                                        produtos: list[index],
+                                        db: db))).then((value) => {
+                                  if (value != null) {initialise()}
+                                });
+                          },
+                          contentPadding: EdgeInsets.only(right: 30, left: 36),
+                          title: Container(
+                            child: Text(
+                              list[index]['name'],
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          leading: Container(
+                              width: 70,
+                              // height: 70,
+                              child:
+                                  Image.network(list[index]['urlImage'] ?? "",
+                                      errorBuilder: (BuildContext context,
+                                          Object exception,
+                                          StackTrace stackTrace) {
+                                return Image.asset(
+                                  'assets/logo.png',
+                                  // fit: BoxFit.cover,
+                                );
+                              })),
+                          subtitle: Text(
+                            list[index]['description'],
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: AppColors.kColorSubtitle,
+                            ),
+                          ),
                         ),
                       ),
                     );
-                  } else {
-                    setState(() {
-                      customIcon = const Icon(Icons.search);
-                      customSearchBar = const Text('Lista de Produtos');
-                    });
-                  }
-                });
-              },
-              icon: const Icon(Icons.search),
-            ),
-
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              child: SizedBox(
-                width: 40,
-                child: FloatingActionButton(
-                  backgroundColor: AppColors.kColotBtnAdd,
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                CadastrarProdutos(db: db))).then((value) {
-                      if (value != null) {
-                        initialise();
-                      }
-                    });
                   },
-                  tooltip: 'Cadastrar um novo produto',
-                  child: Icon(
-                    Icons.add,
-                  ),
-                ),
-              ),
-            ), // This t
-          ],
-        ),
-        body: FutureBuilder(
-            future: db.read(),
-            builder: (context, index) {
-              if (index.connectionState == ConnectionState.done) {
-                if (index.data == null) {
-                  return Center(child: Text("Erro ao carregar dados"));
-                } else {
-                  if (docs.isEmpty == 0) {
-                    return Center(child: Text("Nenhum produto encontrado"));
-                  }
-
-                  List list = [];
-
-                  if (docs.isNotEmpty) {
-                    for (dynamic item in docs) {
-                      String name = item['name'].toString().toLowerCase();
-                      if (name.contains(searchResult.toLowerCase())) {
-                        list.add(item);
-                      }
-                    }
-                  } else {
-                    list.addAll(docs);
-                  }
-                  return ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return new Container(
-                        padding: new EdgeInsets.only(right: 13.0),
-                        child: Card(
-                          semanticContainer: true,
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          elevation: 5,
-                          color: AppColors.kColorCard,
-                          margin: EdgeInsets.all(10),
-                          child: ListTile(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => EditarProduto(
-                                          produtos: list[index],
-                                          db: db))).then((value) => {
-                                    if (value != null) {initialise()}
-                                  });
-                            },
-                            contentPadding:
-                                EdgeInsets.only(right: 30, left: 36),
-                            title: Container(
-                              child: Text(
-                                list[index]['name'],
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                            leading: Container(
-                                width: 70,
-                                // height: 70,
-                                child:
-                                    Image.network(list[index]['urlImage'] ?? "",
-                                        errorBuilder: (BuildContext context,
-                                            Object exception,
-                                            StackTrace stackTrace) {
-                                  return Image.asset(
-                                    'assets/logo.png',
-                                    // fit: BoxFit.cover,
-                                  );
-                                })),
-                            subtitle: Text(
-                              list[index]['description'],
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: AppColors.kColorSubtitle,
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                }
+                );
               }
-              return Center(child: CircularProgressIndicator());
-            }));
+            }
+
+            return Center(child: CircularProgressIndicator());
+          }),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(right: 10),
+        child: SizedBox(
+          width: 40,
+          child: FloatingActionButton(
+            backgroundColor: AppColors.kColotBtnAdd,
+            onPressed: () {
+              Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => CadastrarProdutos(db: db)))
+                  .then((value) {
+                if (value != null) {
+                  initialise();
+                }
+              });
+            },
+            tooltip: 'Cadastrar um novo produto',
+            child: Icon(
+              Icons.add,
+            ),
+          ),
+        ),
+      ), // This t
+    );
   }
 }
